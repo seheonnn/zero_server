86dec07ba481eddac2ef0d0b2d96943120a0ba2c
Jaeyoung Lim <limjyjustin@gmail.com>
2023-11-25 17:20:50 +0900

    Fix: UserController#join 메서드 코드 수정

--- src/main/java/dev/neordinary/zero/controller/UserController.java
+++ src/main/java/dev/neordinary/zero/controller/UserController.java
@@ -4,6 +4,7 @@ import dev.neordinary.zero.dto.UserRequest;
 import dev.neordinary.zero.dto.UserResponse;
 import dev.neordinary.zero.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    
+
+    @PostMapping("/api/v1/user")
     public UserResponse.UserJoinRes join(@RequestBody UserRequest.UserJoin userJoin) {
         return userService.join(userJoin);
     }
