bc2f2902ae62653d5a8d0edf965315a8a98996f1
Jaeyoung Lim <limjyjustin@gmail.com>
2023-11-25 22:15:16 +0900

    FEAT: 기록 기능 추가

--- src/main/java/dev/neordinary/zero/domain/UserEntity.java
+++ src/main/java/dev/neordinary/zero/domain/UserEntity.java
@@ -2,16 +2,21 @@ package dev.neordinary.zero.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Builder
 @Entity
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
 public class UserEntity extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "user_id")
+    private Long user_id;
     private String name;
     private Integer height;
     private Integer weight;
---
+++ src/main/java/dev/neordinary/zero/service/NoteService.java
@@ -0,0 +1,25 @@
+package dev.neordinary.zero.service;
+
+import dev.neordinary.zero.converter.NoteConverter;
+import dev.neordinary.zero.domain.NoteEntity;
+import dev.neordinary.zero.domain.NoteRepository;
+import dev.neordinary.zero.domain.UserEntity;
+import dev.neordinary.zero.domain.UserRepository;
+import dev.neordinary.zero.dto.NoteRequest;
+import dev.neordinary.zero.dto.NoteResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class NoteService {
+
+    private final NoteRepository noteRepository;
+    private final UserRepository userRepository;
+
+    public NoteResponse.NoteJoinRes createNote(Long userId, NoteRequest.NoteJoin noteJoin) {
+        UserEntity currentUser = userRepository.findById(userId).orElseThrow(() -> null);
+        NoteEntity newNote = NoteConverter.toNote(noteJoin, currentUser);
+        return NoteConverter.toNoteDto(noteRepository.save(newNote));
+    }
+}
