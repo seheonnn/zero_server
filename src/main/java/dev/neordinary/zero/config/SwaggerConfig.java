e021d67d86aa156d02c53dcb138d4d1a1ee14b6c
seheonnn <ho78901@naver.com>
2023-11-25 15:19:49 +0900

    FIX: Dockerfile 수정

--- src/main/java/dev/neordinary/zero/config/SwaggerConfig.java
+++ src/main/java/dev/neordinary/zero/config/SwaggerConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     // http://localhost:8080/swagger-ui/index.html#/
     @Bean
     public OpenAPI UMCstudyAPI() {
